ed2ca9277073a118002a049c27e11385d90ffdc9
Ajay Kumar <Ajay847303@gmail.com>
2025-08-21 02:30:55 +0530

    fix: cards

---
+++ app/(routes)/dashboard/_compenents/AiToolCard.tsx
@@ -0,0 +1,31 @@
+import { Button } from '@/components/ui/button'
+import { Link } from 'lucide-react'
+import React from 'react'
+
+interface TOOL{
+  name: string,
+  desc: string,
+  icon:string,
+  button:string,
+  path:string
+}
+
+type AiToolCardProps = {
+  tool: TOOL
+}
+
+function AiToolCard({tool}: AiToolCardProps) {
+  return (
+    <div className='p-5 border rounded-xl hover:shadow-lg transition-all duration-300'>
+    <img src={tool.icon} width={40} height={40} alt={tool.name}/>   
+<h2 className='font-bold mt-3'>{tool.name}</h2>
+<p className='text-gray-400'>{tool.desc}</p>
+{/* <Link href={tool.path}> */}
+<Button className='w-full mt-3'>{tool.button}</Button>
+{/* </Link> */}
+
+    </div>
+  )
+}
+
+export default AiToolCard
