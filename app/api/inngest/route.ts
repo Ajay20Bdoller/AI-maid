ddeb620736c3ddc4ff4a5eabb81f40549f1ad237
Ajay Kumar <Ajay847303@gmail.com>
2025-08-26 22:37:25 +0530

    update routes

---
+++ app/api/inngest/route.ts
@@ -0,0 +1,16 @@
+import { serve } from "inngest/next";
+// Update the import path if your inngest client is located elsewhere, for example:
+import { inngest } from "@/inngest/client";
+
+import { helloWorld } from "@/inngest/functions";
+// Or create the file './inngest/client.ts' with the following content:
+// export const inngest = /* your inngest client initialization */;
+
+// Create an API that serves zero functions
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+     helloWorld
+    /* your functions will be passed here later! */
+  ],
+});
