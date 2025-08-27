883958493048193eed13eb376cd28af2a1303a88
Ajay Kumar <Ajay847303@gmail.com>
2025-08-27 22:35:17 +0530

    update ai-chat page

--- app/(routes)/ai-tools/ai-chat/page.tsx
+++ app/(routes)/ai-tools/ai-chat/page.tsx
@@ -1,20 +1,45 @@
 "use client"
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import { Send } from 'lucide-react'
-import React, { useState } from 'react'
+import { LoaderCircle, Send } from 'lucide-react'
+import React, { useEffect, useState } from 'react'
 import EmptyState from '@/app/(routes)/ai-tools/_components/EmptyState';
 
 import Link from 'next/link'
 import axios from 'axios'
 
+type messages={
+  content:string,
+  role:string,
+  type:string
+}
+
+
 function AiChat() {
 
-  const [userInput, setUserInput] = useState<string>();
+  const [userInput, setUserInput] = useState<string>('');
 const [loading, setLoading]=useState(false);
+const [messageList, setMessageList] = useState<messages[]>([{
+  content:'User Msg',
+  role:'user',
+  type:'text'
+},
+{
+  content:'Assistant Msg',
+  role:'assistant',
+  type:'text'
+}
+]);
+
+
 
 const onSend=async()=>{
   setLoading(true);
+  setMessageList(prev=> [...prev,{
+    content:userInput,
+    role:'user',
+    type:'text'
+  }])
 
 
 
@@ -23,8 +48,21 @@ const result=await axios.post('/api/ai-career-chat-agent',{
 });
 
 console.log(result.data);
+  setMessageList(prev=>[...prev,result.data])
 setLoading(false);
 }
+
+console.log(messageList);
+
+useEffect(()=>{
+  //same mesg into DB
+
+},[messageList])
+
+
+
+
+
   return (
     <div className='px-10 md:px-24 lg:px-36 xl:px-48'>
       <div className='flex items-center justify-between gap-8'>
@@ -36,14 +74,27 @@ setLoading(false);
 <Button>+ New Chat</Button>
 </Link>
     </div>
-    <div className='flex flex-col h-[75vh]'>
+    <div className='flex flex-col h-[75vh]'> {messageList?.length <=0 &&
     <div className='mt-5'>
       {/* empty state operation */}
       <EmptyState selectedQuestion={(question:string) => setUserInput(question) } />
-    </div>
+    </div>}
 
     <div className='flex-1'>
       {/* message list */}
+      {messageList?.map((message, index)=>(
+        <div>
+        <div key={index} className={`flex mb-2 ${message.role=='user'?'justify-end':'justify-start'}`}>
+          <div className={`p-3 rounded-lg gap-2 ${message.role=='user'?'bg-gray-200 text-black rounded-lg':'bg-gray-50 text-black'}`}>
+            {message.content}
+            </div>
+            </div>
+          <div className='flex justify-start p-3 rounded-lg gap-2 bg-gray-50 text-black mb-2'>
+{loading&&messageList?.length-1==index && <LoaderCircle className='animate-spin'/>}
+          {message.content}
+        </div>
+        </div>
+      ))}
     </div>
 
     <div className='flex justify-between items-center'>
