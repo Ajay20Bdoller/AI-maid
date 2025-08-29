560fdce2ef7ec1f8e2b9e5c5c6921ecb349a546a
Ajay Kumar <Ajay847303@gmail.com>
2025-08-30 03:46:02 +0530

    fix: bugs

--- configs/schema.ts
+++ configs/schema.ts
@@ -3,8 +3,9 @@ export const usersTable = pgTable("users", {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
     name: varchar({ length: 255 }).notNull(),
     email: varchar({ length: 255 }).notNull().unique(),
-
+    selectedComponentId: varchar({ length: 255 })   // 👈 yeh naya column
 });
+
 
 export const HistoryTable = pgTable('historyTable',{
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
